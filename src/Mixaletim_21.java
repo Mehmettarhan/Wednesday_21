d0506567fc928b91e28d4aae2280cc9b788f17cf
Mehmettarhan <87024517+Mehmettarhan@users.noreply.github.com>
2021-07-21 16:56:16 -0500

    Added a new integers

---
+++ src/Mixaletim_21.java
@@ -0,0 +1,9 @@
+public class Mixaletim_21 {
+
+    public static void main(String[] args) {
+        System.out.println("Today is a nice day!");
+
+        System.out.println("Adnan");
+        System.out.println("12345678");
+    }
+}
